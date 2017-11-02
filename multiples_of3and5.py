75e7b4de8cb2010c4149b162930131cc6deb5bfe
kukunla <natalia.gerashchenko@gmail.com>
2017-11-02 19:53:07 -0400

    Initial commit

---
+++ multiples_of3and5.py
@@ -0,0 +1,12 @@
+def counting():
+    i = 1000
+    result = 0
+    while i > 2:
+        i -= 1
+        k = i % 5
+        m = i % 3
+        if k == 0 or m == 0:
+            result += i
+    return result
+
+print(counting())
