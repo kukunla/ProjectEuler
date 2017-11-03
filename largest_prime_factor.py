3971aecc198f1bfe4a3e5115705b9192e173a29d
kukunla <natalia.gerashchenko@gmail.com>
2017-11-03 12:19:11 -0400

    new task solved

---
+++ largest_prime_factor.py
@@ -0,0 +1,26 @@
+def finding_prime_factor(x):
+    largest_prime_factor = 0
+    if x % 2 == 0:
+        x = x / 2
+    number = 3
+    while number <= x:
+        number += 2
+        if finding_prime_number(number) and x % number == 0:
+            largest_prime_factor = number
+            x = x / number
+    return largest_prime_factor
+
+
+def finding_prime_number(number):
+    if number % 2 == 0:
+        return False
+    i = 3
+    while i < number / 2:
+        if number % i == 0:
+            return False
+        i += 2
+    return True
+
+
+if __name__ == '__main__':
+    print(finding_prime_factor(600851475143))
